8f47a6806e86ba369dc84b944fd81b2617b185f3
Adem zerii <adem.zeri1234@gmail.com>
2024-05-24 11:32:54 +0100

    update response format

--- server/src/auth/routes/authRoutes.js
+++ server/src/auth/routes/authRoutes.js
@@ -6,7 +6,7 @@ const refreshTokenSchema = require("../schemas/refreshTokenSchema");
 require("dotenv").config();
 
 async function routes(fastify, options) {
-  fastify.post("/login", authController.login);
+  fastify.post("/login", loginSchema, authController.login);
 
   fastify.post("/register", registerSchema, authController.register);
   fastify.post(
