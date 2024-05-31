f94192b808f672ff352f3d449943a22497ebedfe
Mohamed-ncib19 <mohamedncib900@gmail.com>
2024-05-31 11:56:16 +0100

    fix : handle refreshtoken

---
+++ client/src/app/(authenticated)/profile/[username]/layout.jsx
@@ -0,0 +1,29 @@
+'use client'
+import { useRouter } from 'next/navigation';
+
+const ProfileLayout = ({ children }) => {
+  const { back } = useRouter();
+
+  const handleGoBack = () => {
+    back();
+  };
+
+  return (
+    <div className="profile-layout-container">
+      <header className="profile-header rounded-5 rounded-md-0 p-4 mb-5">
+        <button
+          onClick={handleGoBack}
+          className="back fs-3 d-flex justify-content-center align-items-center text-white font-bold rounded-4 border-0 px-3"
+        >
+          <span><i class="bi bi-arrow-left"></i> Back</span>
+        </button>
+        <p className="text-center h1 mb-5 font-bold text-white">Profile</p>
+      </header>
+      <main className="profile-content">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default ProfileLayout;
--- client/src/app/(authenticated)/profile/[username]/page.jsx
+++ client/src/app/(authenticated)/profile/[username]/page.jsx
@@ -1,61 +1,40 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { getSession, useSession } from 'next-auth/react';
+import { getSession } from 'next-auth/react';
 import Link from 'next/link';
 import axios from 'axios';
 
-import ArrowLeftIcon from '@/../public/icons/arrows/arrow-left-icon';
 import { Avatar } from '@/app/(authenticated)/_components/Avatar';
 
 const ProfileCard = ({ params }) => {
   const username = params.username;
-  const { back } = useRouter();
 
   const [user, setUser] = useState({});
-  const [userSession, setUserSession] = useState(null);
+  const [userSession, setUserSession] = useState(false);
 
+  const getUser = async (username) => {
+    try {
+      const response = await axios.get(`/users/${username}`);
+      if (response && response.data) {
+        const fetchedUser = response.data.user;
+        setUser(fetchedUser);
 
-
-const getUser = async (username) =>{
-  try {
-    const response = await axios.get(`/users/${username}`);
-    if(response){
-      setUser(response.data.user);
-      const session = await getSession();
-      setUserSession(
-        session.username === user.username
-      )
-
+        const session = await getSession();
+        setUserSession(session.username === fetchedUser.username);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-  useEffect(() => {
-
-    getUser(username);
-
-  }, []);
-
-  const handleGoBack = () => {
-    back();
   };
 
-  return (
-    <>
-      <div className="profile-header rounded-5 rounded-md-0 p-4 mb-5">
-        <button
-          onClick={handleGoBack}
-          className="back fs-3 d-flex justify-content-center align-items-center text-white font-bold rounded-4 border-0 px-3"
-        >
-          <ArrowLeftIcon /> Back
-        </button>
-        <p className="text-center h1 mb-5 font-bold text-white">Profile</p>
-      </div>
+  useEffect(() => {
+    if (username) {
+      getUser(username);
+    }
+  }, [username]);
 
-      <div className="profile-card d-flex flex-column gap-4 bg-white rounded-4 p-4 py-5 my-5 mt-3 col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 mx-auto shadow-sm">
+  return (
+      <div className="profile-card d-flex flex-column gap-4 bg-white rounded-4 p-4  col-12 col-sm-10 col-md-8 col-lg-6 col-xl-4 mx-auto shadow-sm">
         <div className="d-flex justify-content-start align-items-center gap-5 mb-4">
           <div className="bg-light p-5 rounded-circle shadow-sm">
             <Avatar
@@ -73,22 +52,22 @@ const getUser = async (username) =>{
 
         <div className="card-section d-flex flex-column gap-3 py-3 px-5 mb-3 border rounded-3">
           <h4 className="fw-semibold">Contact Information</h4>
-          <div className="d-flex flex-md-column justify-content-between">
-            <div className="d-flex align-items-center">
-              <i className="bi bi-phone-fill me-2"></i>
+          
+          <div className="d-flex flex-md-row flex-column justify-content-between align-items-center">
+            <div className="d-flex align-items-center gap-3">
+              <i className="bi bi-envelope fs-3 text-muted "></i>
               <p className="mb-0 text-secondary fw-bold">Email</p>
             </div>
             <p className="mb-0 custom-value-field">{user?.email || ''}</p>
           </div>
 
-          <div className="d-flex justify-content-between mt-2">
-            <div className="d-flex align-items-center">
-              <i className="bi bi-phone-fill me-2"></i>
+          <div className="d-flex flex-md-row flex-column justify-content-between align-items-center">
+          <div className="d-flex align-items-center gap-3">
+            <i className="bi bi-whatsapp fs-3 text-muted"></i>
               <p className="mb-0 text-secondary fw-bold">Phone Number</p>
             </div>
             <p className="mb-0 custom-value-field">{user?.phone || ''}</p>
           </div>
-
         </div>
 
         <div className="card-section d-flex flex-column gap-3 py-3 px-5 mb-3 border rounded-3">
@@ -108,23 +87,24 @@ const getUser = async (username) =>{
 
         <div className="card-section d-flex flex-column gap-3 py-3 px-5 mb-3 border rounded-3">
           <h4>System</h4>
-          <div className="d-flex justify-content-between">
-            <div className="d-flex align-items-center">
-              <i className="bi bi-person-fill me-2"></i>
+
+          <div className="d-flex flex-md-row flex-column justify-content-between align-items-center">
+            <div className="d-flex align-items-center gap-3">
+              <i className="bi bi-person fs-3 text-muted"></i>
               <p className="mb-0 text-secondary fw-bold">Username</p>
             </div>
             <p className="mb-0 custom-value-field">{username}</p>
           </div>
-          <div className="d-flex justify-content-between mt-2">
-            <div className="d-flex align-items-center">
-              <i className="bi bi-people-fill me-2"></i>
+
+          <div className="d-flex flex-md-row flex-column justify-content-between align-items-center">
+          <div className="d-flex align-items-center gap-3">
+            <i className="bi bi-person-gear fs-3 text-muted"></i>
               <p className="mb-0 text-secondary fw-bold">User Role</p>
             </div>
             <p className="mb-0 custom-value-field">{user?.role || ''}</p>
           </div>
         </div>
       </div>
-    </>
   );
 };
 
