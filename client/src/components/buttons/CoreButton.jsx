a2af18e126584868ccf599bafec7a403ad7d99c0
Mohamed-ncib19 <mohamedncib900@gmail.com>
2024-07-01 22:01:20 +0100

    add : team member dashboard layout

--- client/src/components/buttons/CoreButton.jsx
+++ client/src/components/buttons/CoreButton.jsx
@@ -3,12 +3,13 @@ const CoreButton = ({
   type,
   onClick,
   variant = 'btn-primary-custom button-custom-css',
+  textColor = 'text-light'
 }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`btn col-lg-6 col-md-8 col-sm-8 col-10 m-auto py-3 ${variant} border-none text-light  d-flex flex-row justify-content-center align-items-center gap-2 `}
+      className={`btn col-lg-6 col-md-8 col-sm-8 col-10 m-auto py-3 ${variant} border-none ${textColor}  d-flex flex-row justify-content-center align-items-center gap-2 `}
     >
       {label}
     </button>
